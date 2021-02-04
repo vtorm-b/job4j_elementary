fe51431febf62368b74fb10204fd41d413c77646
vtorm-b <vtorm-b@mail.ru>
2021-02-04 16:25:44 +0800

    4.1. Аргументы [#464597]

---
+++ src/ru/job4j/calculator/ArgMethod.java
@@ -0,0 +1,14 @@
+package ru.job4j.calculator;
+
+public class ArgMethod {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Sergey Gerasimov";
+        int age = 44;
+
+        ArgMethod.hello(name + ", " + age + " года");
+    }
+}
