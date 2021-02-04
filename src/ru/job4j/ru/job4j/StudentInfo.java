2817b809b6c9b5ae0be17ad20f5cb9d730424219
vtorm-b <vtorm-b@mail.ru>
2021-02-04 14:12:26 +0800

    3.3. Разбор первой программы. [#464606]

--- src/ru/job4j/ru/job4j/StudentInfo.java
+++
@@ -1,9 +0,0 @@
-package ru.job4j.ru.job4j;
-
-public class StudentInfo {
-    public  static void main (String [] args ) {
-        System.out.println("Sergey Gerasimov");
-        System.out.println("04.02.2021");
-
-    }
-}
