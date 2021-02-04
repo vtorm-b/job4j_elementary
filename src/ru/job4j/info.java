945069aa54972288012383e3c274f223a449b198
vtorm-b <vtorm-b@mail.ru>
2021-02-04 14:13:16 +0800

    3.4. Заголовок в файле java [#464607]

--- src/ru/job4j/info.java
+++ src/ru/job4j/info.java
@@ -2,6 +2,6 @@ package ru.job4j;
 
 public class info {
     public static void main (String [] args){
-        System.out.println("04.02.2021");
+        System.out.println("04.02.2021г.");
     }
 }
