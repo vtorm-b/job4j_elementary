64aa949cd0be1bfac6cb6206c662d72aa4afae9d
vtorm-b <vtorm-b@mail.ru>
2021-02-09 12:15:53 +0800

    9.1. String [#464596]

---
+++ src/ru/job4j/calculator/Out.java
@@ -0,0 +1,13 @@
+package ru.job4j.calculator;
+
+public class Out {
+    public static void main(String [] args) {
+       String idea = "I like Java 'But l am a newbie.' ";
+       System.out.println (idea);
+       int year = 2021;
+       idea = idea + year;
+       System.out.println (idea);
+
+
+    }
+}
