c7b7a6a0b2b041ab2dfd25d9c516e9669da7163d
vtorm-b <vtorm-b@mail.ru>
2021-02-09 12:57:54 +0800

    9.1. String [#464596]

--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,14 +1,15 @@
 package ru.job4j.calculator;
 
 public class Calculator {
-    public static void plus (int first, int second) {
+    public static void plus(int first, int second) {
         int result = first + second;
-                System.out.println (result);}
+        System.out.println(result);
+    }
 
-        public static void main (String [] args ) {
-        Calculator.plus(100,500);
-        Calculator.plus(4,2);
-        Calculator.plus(3,5);
+    public static void main(String[] args) {
+        Calculator.plus(100, 500);
+        Calculator.plus(4, 2);
+        Calculator.plus(3, 5);
 
 
     }
