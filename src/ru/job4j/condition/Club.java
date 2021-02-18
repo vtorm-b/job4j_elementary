288ef5aac6574bf410774fa40ad481c657bef7e5
vtorm-b <vtorm-b@mail.ru>
2021-02-18 10:07:57 +0800

    4.3.2. Оператор || [#464540]

---
+++ src/ru/job4j/condition/Club.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+public class Club {
+    public static void permission(boolean hasManey, boolean byFriend) {
+        hasManey = true;
+        byFriend = true;
+        if (hasManey || byFriend) {
+            System.out.println("I cant go to the club.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+     public  static void main(String[]args) {
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
+     }
+}
