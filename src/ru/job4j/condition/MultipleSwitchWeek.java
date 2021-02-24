890037f77329f144f5e2d41da9e472e972b9dced
vtorm-b <vtorm-b@mail.ru>
2021-02-24 13:07:06 +0800

    4.6.2. Switch - matcher [#464543]

--- src/ru/job4j/condition/MultipleSwitchWeek.java
+++ src/ru/job4j/condition/MultipleSwitchWeek.java
@@ -3,8 +3,14 @@ package ru.job4j.condition;
 public class MultipleSwitchWeek {
     public int numberOfDay (String name) {
         return switch (name) {
-            
+            case "понедельник","Monday" -> 1;
+            case "вторник","Tuesday" -> 2;
+            case "среда","Wednesday" -> 3;
+            case "четверг","Thursday" -> 4;
+            case "пятница","Friday" -> 5;
+            case "суббота","Saturday" -> 6;
+            case "воскресенье","Sunday" -> 7;
+            default -> -1;
         }
-
     }
 }
