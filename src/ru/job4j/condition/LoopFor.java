7de5bd7fc9e05573d43ead7d6ca7c2e890634e0c
vtorm-b <vtorm-b@mail.ru>
2021-02-26 11:17:24 +0800

    5.0. Цикл for. Определение. [#464653]

--- src/ru/job4j/condition/LoopFor.java
+++ src/ru/job4j/condition/LoopFor.java
@@ -3,7 +3,7 @@ package ru.job4j.condition;
 public class LoopFor {
     public static void main(String[] args) {
         for (int index = 5; index <=10; index ++) {
-            System.out.println(index);
+            System.out.println(index); 
         }
     }
 }
