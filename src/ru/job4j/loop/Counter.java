a540e7801d5e152409bb1ca8e0a19de429339fa8
vtorm-b <vtorm-b@mail.ru>
2021-02-26 11:59:24 +0800

    5.0.1. Сумма чисел. [#464648]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -3,14 +3,15 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for () {
-            sum = sum +;
-            return sum;
+        for (int index = start; index <= finish; index++) {
+            sum = sum + index;
         }
-        public static void main(String[] args) {
-            System.out.println(sum(0, 10));
-            System.out.println(sum(3, 8));
-            System.out.println(sum(1, 1));
-        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 5));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
     }
 }
