c139d71ae4333ab76769e6fab2d1a2841169476c
vtorm-b <vtorm-b@mail.ru>
2021-03-02 17:17:21 +0800

    5.5. Простое число [#464646]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
        public static boolean check(int number) {
            boolean prime = number >1; //преверяем,что число>1
-           int i;
-           for (i = 2; i < number; i++) {
+           //int i;
+           for (int i = 2; i < number; i++) {
                if (number % i == 0) {
                    prime = false;
                    break;
--- src/ru/job4j/loop/Fitness.java
+++ src/ru/job4j/loop/Fitness.java
@@ -3,9 +3,7 @@ package ru.job4j.loop;
 public class Fitness {
     public static int calc(int ivan, int nic) {
         int month = 0;
-        int powIvan = ivan;
-        int powNic = nic;
-            while (powIvan < powNic) {
+        while (ivan < nic) {
             ivan *= 3;
             nic *= 2;
             month++;
