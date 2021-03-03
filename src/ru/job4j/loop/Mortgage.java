88e932302013abdef97167065d9e908d52959e56
vtorm-b <vtorm-b@mail.ru>
2021-03-03 14:02:41 +0800

    5.6. Простые числа [#464644]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year (double amount, double salary, double percent) {
+        int year = 0;
+        while (amount < salary) {
+            amount += (amount  * (percent / 100));
+            amount -= salary;
+            year++;
+            }
+        return year;
+        }
+}
+
