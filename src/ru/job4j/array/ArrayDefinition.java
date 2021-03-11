e89aa5f136916c30e05824b7a9d04b6e30aec080
vtorm-b <vtorm-b@mail.ru>
2021-03-11 14:59:43 +0800

    6.0.1. Объявление массива. [#464582]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -3,8 +3,9 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
-        String[] surnumes = new[100500];
-        float[] prices = new[40];
+        String[] surnumes = new String[100500];
+        float[] prices = new float[40];
+        System.out.println("Размер массива равен " + ages.length +" "+ surnumes.length +" "+ prices.length);
     }
 
 }
