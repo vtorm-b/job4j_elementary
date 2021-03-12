be6c68540f6ccc8db227bac58b97c6a77dd6e3f3
vtorm-b <vtorm-b@mail.ru>
2021-03-12 10:25:15 +0800

    6.0.2. Заполнение массива. [#464567]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,11 +1,17 @@
 package ru.job4j.array;
 
+import static java.lang.System.*;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
-        short[] ages = new short[10];
-        String[] surnumes = new String[100500];
-        float[] prices = new float[40];
-        System.out.println("Размер массива равен"+"\n" + ages.length +"\n"+ surnumes.length +"\n"+ prices.length);
+        String[] names = new String[4];
+        names[0] = "Sergey";
+        names[1] = "Ivan";
+        names[2] = "Petr";
+        names[3] = "Andrey";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
-
 }
