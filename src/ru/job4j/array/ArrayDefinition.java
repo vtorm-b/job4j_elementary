28f66560fea9a4d0c7800c0d2d3091f5734922ec
vtorm-b <vtorm-b@mail.ru>
2021-03-15 15:57:41 +0800

    6.0.1.1. Размер массива [#464578]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -5,13 +5,13 @@ import static java.lang.System.*;
 public class ArrayDefinition {
     public static void main(String[] args) {
         String[] names = new String[4];
-        names[0] = "Sergey";
+       /* names[0] = "Sergey";
         names[1] = "Ivan";
         names[2] = "Petr";
         names[3] = "Andrey";
         System.out.println(names[0]);
         System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        System.out.println(names[2]);*/
+        System.out.println("Размер массива равен: "+names.length);
     }
 }
---
+++ src/ru/job4j/array/FindLoop.java
@@ -0,0 +1,14 @@
+/*package ru.job4j.array;
+
+public class FindLoop {
+    public static int indexOf(int[] data, int el) {
+        int rst = -1; //если элемента нет в массиве
+        for (int i = 0; i < el; i++) {
+            if(data[index] == el) {
+                rst = index;
+                break;
+            }
+        }
+        return rst;
+    }
+}*/
