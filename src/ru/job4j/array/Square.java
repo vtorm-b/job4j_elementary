f8ece07e0418c2f84e2f72336fcd61f4a8771910
vtorm-b <vtorm-b@mail.ru>
2021-03-15 08:46:28 +0800

    6.0.5. Массивы в тестах. [#464569]

--- src/ru/job4j/array/Square.java
+++ src/ru/job4j/array/Square.java
@@ -11,7 +11,7 @@ public class Square {
     }
 
     public static void main(String[] args) {
-        int[] array = calculate(4);
+        int[] array = calculate(5);
         for (int index = 0; index < array.length; index++) {
             System.out.println(array[index]);
         }
